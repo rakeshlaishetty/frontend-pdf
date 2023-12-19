6fe687a7805bff7439595480c06df4c3e2aa9c64
Rakesh laishetty <rakeshlaishetty123@gmail.com>
2023-12-19 12:37:08 +0530

    added webpack configuration

---
+++ babel.config.js
@@ -0,0 +1,11 @@
+const isDevelopment = process.env.NODE_ENV !== 'production';
+module.exports = {
+    "presets": [
+        "@babel/preset-env",
+        ["@babel/preset-react", { runtime: 'automatic' }]
+    ],
+    "plugins": [
+        isDevelopment && "react-refresh/babel",
+        // ... other plugins
+    ].filter(Boolean)
+}
