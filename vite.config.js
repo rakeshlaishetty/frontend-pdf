ce0a5c12a2ca8e45d70c693220e826da4552bada
Rakesh laishetty <rakeshlaishetty123@gmail.com>
2024-01-03 00:16:27 +0530

    changes done

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,5 @@ import react from '@vitejs/plugin-react'
 console.log(process.env.NODE_ENV,'process.env.NODE_ENV')
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:process.env.NODE_ENV === 'production' ? '/frontend-pdf/' : '/',
   plugins: [react()],
 })
