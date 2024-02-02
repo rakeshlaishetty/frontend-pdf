7e42c26c5990b0a3b179c4e3dcc5b8791c63af10
Manoj Kumar <manoj.kumar4@emerson.com>
2024-02-02 16:13:53 +0530

    new theme installed

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-console.log(process.env.NODE_ENV,'process.env.NODE_ENV')
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
